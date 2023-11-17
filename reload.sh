a44e63c9eafac1335df217c4e02b3d91a3d01317
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-11-16 23:45:23 -0600

    Finished temporary reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if ( $(git branch | grep '*' | awk '{ print $2 }') == 'dev-docker'); then
-	echo "Already in branch!"
+if [ $(git branch | grep '*' | awk '{ print $2 }') == 'dev-docker' ]
+then
+	echo "Currently in correct branch!"
 else
 	echo "Changing to 'dev-docker'"
 	git checkout 'dev-docker'
@@ -8,8 +9,6 @@ fi
 
 git pull
 git reset --hard
-
-return 0
 
 docker build -t Owltech-Vite .
 docker run -d -p 5173:5173 --name Owltech-Vite-App Owltech-Vite
