ac175d8abfaefcabcddf6e9ac3fed4dfeb85b5f9
AnaJmz345 <anajmzse@gmail.com>
2023-11-20 19:46:24 -0600

    added designed band and inferior purple background

--- src/components/Robots/Robots.jsx
+++ src/components/Robots/Robots.jsx
@@ -5,7 +5,14 @@ export default function Robots() {
   return (
     <div className="robotsWrapper">
       <div className="topFrame">Conoce a los robots</div>
-      <div className="designFrame" />
+      <div className="designFrame" style={{ backgroundImage: `url("src/assets/1.png")`}}/>
+      
+      <div style={{backgroundColor: "#34052F", width:"100%", height:"80vh"}}>
+      
+      </div>
+
+
     </div>
   );
 }
+
