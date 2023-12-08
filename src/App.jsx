2546f4b788ee82f884858e93d9987d56156e334d
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-12-08 17:00:19 -0600

    Cleaned app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import WhatIs from "./components/WhatIs/WhatIs";
-import TopBar from "./components/TopBar";
+import TopBar from "./components/TopBar/TopBar";
 import MisionVision from "./components/MisionVision/MisionVision";
 import Robomaster from "./components/Robomaster/Robomaster";
 import Members from "./components/Members/Members";
 import Robots from "./components/Robots/Robots";
 import Sponsors from "./components/Sponsors/Sponsors";
-import Contact from "./components/Contact/Contact";
+// import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <>
       <TopBar />
       <WhatIs />
-      <MisionVision />
+      {/* <MisionVision />
       <Robomaster />
       <Members />
       <Robots />
       <Sponsors />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
