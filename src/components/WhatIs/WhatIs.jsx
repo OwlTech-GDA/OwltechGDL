db2074e56f415b8cf4e422d695d4b68e2043e51c
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-12-08 17:24:35 -0600

    [WhatIs] Started

--- src/components/WhatIs/WhatIs.jsx
+++ src/components/WhatIs/WhatIs.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./WhatIs.css";
+import logo from "../../assets/Logo 1.png";
 
 export default function WhatIs() {
-  return <div>WhatIs</div>;
+  return (
+    <div className="whatis-body">
+      <div
+        className="img-side"
+        style={{ backgroundImage: `url("../../assets/whatit-bg.png")` }}
+      >
+        <img src={logo} />
+      </div>
+      <div className="text-side">
+        <h2>¿Qué es OwlTech?</h2>
+        <p>
+          Owltech es un grupo estudiantil que se enfoca en la competencia
+          Robomaster en la cual sus miembros pueden desarrollar hábilidades
+          relacionadas a los temas robótica, programación, electrónica y
+          mecánica
+        </p>
+      </div>
+    </div>
+  );
 }
