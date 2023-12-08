1d824bae503dc21eb5c4bd26cf8ae4fe3855e9df
AnaJmz345 <anajmzse@gmail.com>
2023-12-08 15:20:28 -0600

    tratar de arreglar la parte de qué es robomaster

--- src/components/Robomaster/Robomaster.jsx
+++ src/components/Robomaster/Robomaster.jsx
@@ -4,14 +4,15 @@ import "./Robomaster.css";
 export default function Robomaster() {
   return (
     <div style={{backgroundColor: "#34052F", width:"100%", height:"100vh",color:"white"}}>
-      <div className="back">
-        <div className="superiorImg" style={{ backgroundImage: `url("src/assets/2.png")`}}></div>
-        <div className="title">¿Qué es Robomaster?</div>
-      </div>
+      <div className="superiorImg" style={{ backgroundImage: `url("src/assets/2.png")`}}>
+        <div className="title" id="title" style={{opacity:1}}>¿Qué es robomaster?</div>
         <div className="description">Cada año, DJI organiza una competencia en la que los equipos participantes diseñan y construyen robots 
         con funciones diversas para competir en un campo de batalla. En esta competencia, los equipos controlan sus robots para atacar a los 
         robots y la base del equipo contrario utilizando proyectiles. Al final del enfrentamiento, el equipo que tenga más puntos restantes 
         en su base gana.</div>
+        <div className="bottomImg" style={{ backgroundImage: `url("src/assets/2.png")`}}></div>
+
+      </div>
 
 
     </div>
